f76a65aa08163b98ab21ce227e43825ebf635b0e
kazuky <zakkya@gmail.com>
2018-04-14 00:58:06 +0200

    1st

--- src/states/game.js
+++ src/states/game.js
@@ -11,6 +11,7 @@ class Game extends Phaser.State {
     // Is the first method to run on the state; its sole
     // purpose is to load the assets you want to use.
     preload() {
+        game.load.spritesheet('dude', 'assets/dude.png', 32, 48, 9);
     }
 
     // =========== STATE CREATION ===========
@@ -20,13 +21,37 @@ class Game extends Phaser.State {
 	console.log("Game!");
 	this.background = this.game.add.image(this.game.width,this.game.height,'background');
 	this.background.angle = 180;
-	this.input.onDown.add(this._startGame, this);
+	//Changer le background : this.input.onDown.add(this._startGame, this);
+    this.bot = game.add.sprite(200, 200, 'dude');
+    this.bot.animations.add('run');
+    this.bot.animations.play('run', 15, true);
+	//Active la physique du jeu
+    this.game.physics.enable(this.bot, Phaser.Physics.ARCADE);
+    this.destinationX=0;
+    this.destinationY=0;
+
+
+
+
     }
 
     // ============= GAME LOOP! =============
     // This is the game loop method! It will be called as close
     // to 60fps in the browser as it can.
     update() {
+    	
+    	//game.physics.arcade.moveToXY(object,x,y,speed,maxTime);
+    	game.physics.arcade.moveToXY(this.bot,this.destinationX,this.destinationY,400);
+
+
+    //game.physics.arcade.moveToPointer(this.bot, 400);
+    if (this.game.input.mousePointer.isDown){
+
+
+    	this.destinationX = game.input.x;
+
+		this.destinationY = game.input.y;
+	}
     }
 
     // ========== PRIVATE METHODS ===========
